57c8fc973b14ed044421c61c674abdc60aee154a
Habibunayka Miftah Al-Rizqi <74962426+habibunayka@users.noreply.github.com>
2025-08-27 10:53:28 +0700

    fix: normalize imports and declare radial deps

--- frontend/src/components/common/ui/forms/ToggleGroup.jsx
+++ frontend/src/components/common/ui/forms/ToggleGroup.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group@1.1.2";
+import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group";
 
 import { cn } from "../utils";
 import { toggleVariants } from "./toggle";
