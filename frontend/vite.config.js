8c0eccd42972c5f680564765ddcd6e55986d79c7
habibunayka <habibunayka@gmail.com>
2025-08-27 11:46:28 +0700

    Add tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@lib": path.resolve(__dirname, "src/lib"),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+            "@components": path.resolve(__dirname, "src/components"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+            "@lib": path.resolve(__dirname, "src/lib"),
+        },
     },
-  },
 });
