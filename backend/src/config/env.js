0911c8eb7708790fe2fe5f389702bb0d7e6a6dfd
Habibunayka Miftah Al-Rizqi <74962426+habibunayka@users.noreply.github.com>
2025-08-27 08:54:08 +0700

    feat: add postgres env configuration

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,6 +6,11 @@ export const env = {
     JWT_SECRET: process.env.JWT_SECRET,
     REDIS_URL: process.env.REDIS_URL,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:5173",
+    DB_HOST: process.env.DB_HOST,
+    DB_PORT: process.env.DB_PORT,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
     S3: {
         ENDPOINT: process.env.S3_ENDPOINT,
         REGION: process.env.S3_REGION,
